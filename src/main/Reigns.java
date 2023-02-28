dd3bb25520d03ea136d0c8d2db09207ec5d26b76
Paul <paul.mestack59@gmail.com>
2023-02-28 17:47:24 +0100

    feat: update Reponse.java

--- src/main/Reigns.java
+++ src/main/Reigns.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Map;
 
 /**
  * La classe Reigns représente le programme principal du jeu Reigns
@@ -17,13 +18,22 @@ public class Reigns {
     private static ArrayList<Question> questions;
 
     /**
-     * La méthode main lance le jeu Reigns. Il initialise les questions, le personnage,
-     * affiche les jauges du personnage et lance une boucle de jeu qui se termine lorsque le personnage perd.
+     * La méthode main lance le jeu Reigns. Il initialise les questions, le
+     * personnage,
+     * affiche les jauges du personnage et lance une boucle de jeu qui se termine
+     * lorsque le personnage perd.
      * Il affiche également le nombre de tours de jeu que le personnage a joué.
      *
      * @param args les arguments de la ligne de commande
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        Reponse reponse = new Reponse();
+        Effet effet1 = new Effet();
+        reponse.remplirReponse("G", effet1);
+        System.out.println(reponse.toString());
+        // afficheReponse(reponse);
+
         System.out.println("Bienvenue sur Reigns");
         System.out.println("Création de la partie et du personnage...");
         Partie partie = new Partie(initBanqueQuestions());
@@ -38,10 +48,20 @@ public class Reigns {
         partie.finDePartie();
     }
 
+    public String afficheReponse(Map<String, Effet> reponses) {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Effet> reponse : reponses.entrySet()) {
+            result.append("Réponse ").append(reponse.getKey()).append(":");
+            result.append(reponse.getValue());
+        }
+        return result.toString();
+    }
+
     /**
-     * Cette fonction initialise la banque de questions. Elle crée les questions et les ajoute à la banque.
+     * Cette fonction initialise la banque de questions. Elle crée les questions et
+     * les ajoute à la banque.
      */
-    public static ArrayList<Question> initBanqueQuestions(){
+    public static ArrayList<Question> initBanqueQuestions() {
         questions = new ArrayList<>();
         Question question1 = new Question(
                 "Main du roi",
@@ -57,7 +77,7 @@ public class Reigns {
                 "Il n'y a plus rien à manger",
                 "Importer de la nourriture",
                 "Ne rien faire");
-        question2.ajouteEffetGauche(TypeJauge.FINANCE,-5);
+        question2.ajouteEffetGauche(TypeJauge.FINANCE, -5);
         question2.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
         question2.ajouteEffetDroite(TypeJauge.PEUPLE, -5);
         questions.add(question2);
--- src/main/Reponse.java
+++ src/main/Reponse.java
@@ -1,5 +1,32 @@
 package main;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Reponse {
-    
+
+    protected Map<String, Effet> reponse;
+
+    public Reponse() {
+        this.reponse = new TreeMap<>();
+    }
+
+    public String afficheReponse(Map<String, Effet> reponses) {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Effet> reponse : reponses.entrySet()) {
+            result.append("Réponse ").append(reponse.getKey()).append(":");
+            result.append(reponse.getValue());
+        }
+        return result.toString();
+    }
+
+    public void remplirReponse(String cote, Effet effet) {
+        this.reponse.put(cote, effet);
+    }
+
+    @Override
+    public String toString() {
+        return "Reponse{" + "reponse=" + reponse + "}";
+    }
+
 }
